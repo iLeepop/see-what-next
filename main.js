d3c1efe4cae8ed1c8f56a6eb6daab9ffd1c7c833
iLeepop <zihao02325@gmail.com>
2024-02-20 15:59:32 +0800

    feat: vite && project

---
+++ main.js
@@ -0,0 +1,31 @@
+import './style.less'
+document.querySelector('#app').innerHTML = `
+  <div id="container" class="">
+    <div id="prev" class="roway out-line">
+      
+    </div>
+    <div id="main" class="out-line">
+      
+      <div id="tools" class="out-line">
+        <ul id="tools-list">
+          <li class="out-line">
+            
+          </li>
+          <li class="out-line">
+            
+          </li>
+          <li class="out-line">
+            
+          </li>
+        </ul>
+      </div>
+      <div id="card" class="out-line">
+        
+      </div>
+    </div>
+    <div id="next" class="roway out-line">
+      
+    </div>
+  </div>
+`
+
