74ff5bfc307363100262b3652eddf1d800194da2
iLeepop <zihao02325@gmail.com>
2024-02-21 18:15:37 +0800

    feat: App singlepage

--- main.js
+++ main.js
@@ -1,29 +1,6 @@
 import './style.less'
 //
+import './src/layout/App.js'
 import './src/components/slider/slider.js'
 
-document.querySelector('#app').innerHTML = `
-  <div id="container" class="">
-    <swn-slider direct="left" width="50px" motion="30px"></swn-slider>
-    <div id="main" class="out-line">
-      
-      <div id="tools" class="out-line">
-        <ul id="tools-list">
-          <li class="out-line">
-            
-          </li>
-          <li class="out-line">
-            
-          </li>
-          <li class="out-line">
-            
-          </li>
-        </ul>
-      </div>
-      <div id="card" class="out-line">
-        
-      </div>
-    </div>
-    <swn-slider direct="right" width="50px" motion="30px"></swn-slider>
-  </div>
-`
+document.querySelector('#app').innerHTML = `<swn-app></swn-app>` // 挂载
---
+++ src/layout/App.js
@@ -0,0 +1,46 @@
+class App extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
+  connectedCallback() {
+    this.render()
+  }
+  disconnectedCallback() { }
+  static get observedAttributes() {
+    return []
+  }
+  attributeChangedCallback(value, oldValue, newValue) { }
+  render() {
+    // 由于全局样式不能影响 shadow dom 元素，如果在 shadow dom 里面导入样式表，那么就会遇到 build 完的文件名不能与这里对上的情况
+    // 看有没有其它更好的方案 可以解决全局样式无法穿透的问题
+    this.shadowRoot.innerHTML = `
+    <link rel="stylesheet" href="./style.less">
+    <div id="container" class="">
+      <swn-slider direct="left" width="50px" motion="30px"></swn-slider>
+      <div id="main" class="out-line">
+        
+        <div id="tools" class="out-line">
+          <ul id="tools-list">
+            <li class="out-line">
+              
+            </li>
+            <li class="out-line">
+              
+            </li>
+            <li class="out-line">
+              
+            </li>
+          </ul>
+        </div>
+        <div id="card" class="out-line">
+          
+        </div>
+      </div>
+      <swn-slider direct="right" width="50px" motion="30px"></swn-slider>
+    </div>
+  `
+  }
+}
+
+customElements.define('swn-app', App)
