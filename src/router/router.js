113188b3d89ddf3c07b4bfa92d57872668469418
iLeepop <zihao02325@gmail.com>
2024-03-05 18:18:47 +0800

    add mark

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import App from '../layout/App.js'
 import About from '../pages/About/About.js'
 import Other from '../pages/Other/Other.js'
 
+// 路由结构错了 应该是个数组 方便写递归
 export const route = {
   '/': {
     name: 'home',
